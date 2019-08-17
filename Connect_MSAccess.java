c7c3207db0db003a9c8250c76b50e9eb49ea7203
Mukesh-Lekhrajani <39569102+Mukesh-Lekhrajani@users.noreply.github.com>
2019-08-17 19:40:10 +0530

    Connecting To MS Access 2013 From Java
    Connecting To MS Access 2013 From Java
    
    I did a lot of R&D on various web sites including the Eclipse, Oracle, StackOverflow, Etc.  but did not find the direct ready code for connecting MS Access from Java.. then, by collating bits and pieces, i somehow made this code and have tested..

---
+++ Connect_MSAccess.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+import net.ucanaccess.jdbc.*;
+
+public class Connect_MSAccess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			   Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C:/JDBC-ODBC/Database1.accdb");
+			   Statement stm = conn.createStatement();
+			   ResultSet rs = stm.executeQuery("Select * From Table1");
+			   while (rs.next())
+			   {
+				   System.out.println(rs.getString("Stu_Name"));
+			   }
+
+			   conn.close();
+			   System.out.println("Connection Closed..Resources Released");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
